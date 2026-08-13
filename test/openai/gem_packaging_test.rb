f18478c8b374c11509abbc883806efcdfb1888ae
Justin Beckwith <jbeckwith@openai.com>
2026-08-13 10:54:08 -0700

    fix: package every README-linked guide and example

--- test/openai/gem_packaging_test.rb
+++ test/openai/gem_packaging_test.rb
@@ -2,20 +2,28 @@
 
 require "rubygems/package"
 require "tmpdir"
+require "uri"
 
 require_relative "test_helper"
 
 class OpenAI::Test::GemPackagingTest < Minitest::Test
-  def test_packages_azure_provider_guide_as_documentation
+  def test_packages_every_relative_readme_link
     specification = Gem::Specification.load(File.expand_path("../../openai.gemspec", __dir__))
+    readme = File.read(File.expand_path("../../README.md", __dir__))
+    relative_links = readme.scan(/\[[^\]]+\]\(([^\s)]+)(?:\s+[^)]*)?\)/).flatten.filter_map do |target|
+      uri = URI.parse(target)
+      uri.path if uri.relative? && uri.host.nil? && !uri.path.empty?
+    end
 
     Dir.mktmpdir("openai-gem-packaging-test") do |directory|
       gem_file = File.join(directory, "openai.gem")
       Gem::Package.build(specification, false, false, gem_file)
       package = Gem::Package.new(gem_file)
 
-      assert(package.contents.include?("azure.md"), "built gem does not include azure.md")
-      assert_includes(package.spec.extra_rdoc_files, "azure.md")
+      assert_empty(relative_links - package.contents, "README links are missing from the built gem")
+
+      linked_guides = relative_links.select { File.extname(_1) == ".md" }
+      assert_empty(linked_guides - package.spec.extra_rdoc_files, "README guides are missing from RDoc")
     end
   end
 end
