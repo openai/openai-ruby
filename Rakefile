78d656c849204bcc831afd333aa431939e3f6375
Alex Chang <apcha@openai.com>
2026-09-08 11:34:37 -0400

    ci: profile Ruby test and subprocess execution

--- Rakefile
+++ Rakefile
@@ -35,7 +35,9 @@ bedrock_tests = FileList["test/openai/providers/bedrock*_test.rb"]
 run_tests = lambda do |files|
   abort("No test files selected") if files.empty?
 
-  rb = files.map { "require_relative(#{_1.dump});" }.join
+  rb = "require_relative 'test/ci_timing'; started = Process.clock_gettime(Process::CLOCK_MONOTONIC);" + files.map { "require_relative(#{_1.dump});" }.join
+
+  rb += "warn format('PROFILE load %.3f', Process.clock_gettime(Process::CLOCK_MONOTONIC) - started);"
 
   ruby(*%w[-w -e], rb, verbose: false) { fail unless _1 }
 end
---
+++ test/ci_timing.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require 'open3'
+$stdout.sync = true
+$stderr.sync = true
+module TestTiming
+  def run
+    started = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+    super
+  ensure
+    elapsed = Process.clock_gettime(Process::CLOCK_MONOTONIC) - started
+    warn format('PROFILE test %.3f %s#%s', elapsed, self.class, name) if elapsed >= 1
+  end
+end
+Minitest::Test.prepend(TestTiming)
+module SubprocessTiming
+  def capture3(*args, **opts)
+    started = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+    super
+  ensure
+    elapsed = Process.clock_gettime(Process::CLOCK_MONOTONIC) - started
+    command = args.first.is_a?(Hash) ? args[1] : args.first
+    warn format('PROFILE subprocess %.3f %s %s', elapsed, File.basename(command.to_s), caller.find { |line| line.include?('_test.rb') }) if elapsed >= 1
+  end
+end
+Open3.singleton_class.prepend(SubprocessTiming)
